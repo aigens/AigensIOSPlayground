f5f4c3631fdb15fdc1ae03a46b6ad5ac03991c45
Peter Liu <peter.liu@aigens.com>
2016-10-07 19:14:57 +0800

    Move manual files

---
+++ AigensIOSPlayground-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AigensIOSPlayground-Bridging-Header.h
+//  AigensIOSPlayground
+//
+//  Created by Peter Liu on 7/10/2016.
+//  Copyright © 2016 Aigens. All rights reserved.
+//
+
+#ifndef AigensIOSPlayground_Bridging_Header_h
+#define AigensIOSPlayground_Bridging_Header_h
+
+
+#endif /* AigensIOSPlayground_Bridging_Header_h */
---
+++ ete.m
@@ -0,0 +1,9 @@
+//
+//  ete.m
+//  AigensIOSPlayground
+//
+//  Created by Peter Liu on 7/10/2016.
+//  Copyright © 2016 Aigens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
